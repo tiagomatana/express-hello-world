05ff192a58006cd047fd1a5514d13086e5fde652
Tiago Matana <tiago.matana@gmail.com>
2023-05-05 13:17:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
+app.get("/api", (req, res) => res.type('json').send({name: 'tiago'}));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
